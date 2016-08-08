e881eaeb6d36525ecbf12f12ee4f4a15d77e97c1
Brian Ho <ho_brian@me.com>
2016-08-08 13:46:45 -0700

    view distance

--- js/reveal.js
+++ js/reveal.js
@@ -154,7 +154,7 @@
 			parallaxBackgroundVertical: 25,
 
 			// Number of slides away from the current that are visible
-			viewDistance: 1,
+			viewDistance: 2,
 
 			// Script dependencies to load
 			dependencies: []
